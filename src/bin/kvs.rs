f3a2832c9f46055bc82928114cd95828b69ba428
claravanstaden <Cats 4 life!>
2022-01-24 11:24:09 +0200

    Project 1 complete

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,35 @@
+use clap::{App, Arg};
+use kvs::KvStore;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("kvs")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("Clara van Staden. <clara@snowfork.com>")
+        .about("Stores key values in memory")
+        .subcommand(App::new("set")
+            .arg(Arg::new("key")
+                .help("print debug information verbosely"))
+            .arg(Arg::new("value")
+                .help("print debug information verbosely")))
+        .subcommand(App::new("get")
+            .about("controls testing features")
+            .arg(Arg::new("key")
+                .help("print debug information verbosely")))
+        .subcommand(App::new("rm")
+            .about("controls testing features")
+            .arg(Arg::new("rm")
+                .help("print debug information verbosely")))
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("set") {
+        unimplemented!("set unimplemented")
+    }
+    else if let Some(matches) = matches.subcommand_matches("get") {
+        unimplemented!("get unimplemented")
+    }
+    else if let Some(matches) = matches.subcommand_matches("rm") {
+        unimplemented!("rm unimplemented")
+    } else {
+        panic!("invalid command")
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,72 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+#[deny(missing_docs)]
+#[derive(Debug)]
+/// Stores key-value pairs in memory.
+pub struct KvStore {
+    store: HashMap<String, String>
+}
+
+impl KvStore {
+    /// Creates an empty new key-value store using a hashmap data
+    /// structure.
+    pub fn new() -> KvStore {
+        KvStore{
+            store: HashMap::new()
+        }
+    }
+
+    /// Sets a key with a value in the store.
+    /// # Examples
+    /// ```
+    /// use kvs::KvStore;
+    /// let mut kvs = KvStore::new();
+    ///
+    /// let key = String::from("foo");
+    /// let value = String::from("bar");
+    ///
+    /// kvs.set(key, value);
+    /// ```
+    pub fn set(&mut self, key: String, value: String) {
+        self.store.insert(key, value);
+    }
+
+    /// Gets a value in the store using the key.
+    /// # Examples
+    /// ```
+    /// use kvs::KvStore;
+    /// let mut kvs = KvStore::new();
+    ///
+    /// let key = String::from("foo");
+    /// let value = String::from("bar");
+    ///
+    /// kvs.set(key, value);
+    ///
+    /// print!(kvs.get(String::from("foo")));
+    /// ```
+    pub fn get(&self, key: String) -> Option<String> {
+        let value = self.store.get(&key);
+
+        match value {
+            None => None,
+            Some(val) => Some(String::from(val)), // not sure if this is right
+        }
+    }
+
+    /// Removes the value and key in the store using the key.
+    /// # Examples
+    /// ```
+    /// use kvs::KvStore;
+    /// let mut kvs = KvStore::new();
+    ///
+    /// let key = String::from("foo");
+    /// let value = String::from("bar");
+    ///
+    /// kvs.set(key, value);
+    ///
+    /// kvs.rm(String::from("foo"));
+    /// ```
+    pub fn remove(&mut self, key: String) {
+        self.store.remove(&key);
+    }
 }
