7f13619ed21b40568e5183844bd4bb3271ffac3f
claravanstaden <Cats 4 life!>
2022-01-25 11:37:39 +0200

    feat: First bunch of tests pass.

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,7 +1,10 @@
+use std::env;
+use std::env::current_dir;
+use std::path::Path;
 use clap::{App, Arg};
-use kvs::KvStore;
+use kvs::{KvStore, Result};
 
-fn main() {
+fn main() -> Result<()> {
     let matches = App::new("kvs")
         .version(env!("CARGO_PKG_VERSION"))
         .author("Clara van Staden. <clara@snowfork.com>")
@@ -20,16 +23,55 @@ fn main() {
         .subcommand(
             App::new("rm")
                 .about("remove a value from the key store")
-                .arg(Arg::new("rm")),
+                .arg(Arg::new("key")),
         )
         .get_matches();
 
+    let mut kvs = KvStore::open(env::current_dir().unwrap())?;
+
     if let Some(matches) = matches.subcommand_matches("set") {
-        unimplemented!("set unimplemented")
+        let key = match matches.value_of("key") {
+            Some(key) => key,
+            None => panic!("unable to read key parameter for command set")
+        };
+
+        let val = match matches.value_of("value") {
+            Some(val) => val,
+            None => panic!("unable to read value parameter for command set")
+        };
+
+        kvs.set(String::from(key), String::from(val))
+
     } else if let Some(matches) = matches.subcommand_matches("get") {
-        unimplemented!("get unimplemented")
+        let key = match matches.value_of("key") {
+            Some(key) => key,
+            None => panic!("unable to read key parameter for command set")
+        };
+
+        match kvs.get(String::from(key)) {
+            Ok(value) => {
+                match value {
+                    Some(val) => {
+                        println!("{}", val);
+                        Ok(())
+                    },
+                    None => Ok(()),
+                }
+            },
+            Err(e) => {
+                println!("{}", e);
+                Ok(())
+            },
+        }
+
     } else if let Some(matches) = matches.subcommand_matches("rm") {
-        unimplemented!("rm unimplemented")
+        let key = match matches.value_of("key") {
+            Some(key) => key,
+            None => panic!("unable to read key parameter for command set")
+        };
+
+        kvs.remove(String::from(key))
+
     } else {
         panic!("invalid command")
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,72 +1,160 @@
 use std::collections::HashMap;
+use std::fs::OpenOptions;
+use std::io;
+use std::path::{Path, PathBuf};
+use failure::{Error, format_err};
+use serde::{Serialize, Deserialize};
+use std::io::prelude::*;
 
 #[deny(missing_docs)]
 #[derive(Debug)]
 /// Stores key-value pairs in memory.
 pub struct KvStore {
     store: HashMap<String, String>,
+    filepath: String,
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+const FILE_NAME: &str = "log.txt";
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Command {
+    key: String,
+    value: String,
+    kind: String,
 }
 
 impl KvStore {
     /// Creates an empty new key-value store using a hashmap data
     /// structure.
-    pub fn new() -> KvStore {
+    pub fn new(filepath: String) -> KvStore {
         KvStore {
             store: HashMap::new(),
+            filepath,
         }
     }
 
-    /// Sets a key with a value in the store.
-    /// # Examples
-    /// ```
-    /// use kvs::KvStore;
-    /// let mut kvs = KvStore::new();
-    ///
-    /// let key = String::from("foo");
-    /// let value = String::from("bar");
-    ///
-    /// kvs.set(key, value);
-    /// ```
-    pub fn set(&mut self, key: String, value: String) {
-        self.store.insert(key, value);
-    }
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {
+        // It then serializes that command to a String
+        let cmd = Command {
+            key,
+            value,
+            kind: String::from("set"),
+        };
 
-    /// Gets a value in the store using the key.
-    /// # Examples
-    /// ```
-    /// use kvs::KvStore;
-    /// let mut kvs = KvStore::new();
-    ///
-    /// let key = String::from("foo");
-    /// let value = String::from("bar");
-    ///
-    /// kvs.set(key, value);
-    ///
-    /// print!(kvs.get(String::from("foo")));
-    /// ```
-    pub fn get(&self, key: String) -> Option<String> {
-        let value = self.store.get(&key);
+        let cmd_string = serde_json::to_string(&cmd)?;
 
-        match value {
-            None => None,
-            Some(val) => Some(String::from(val)), // not sure if this is right
+        // It then appends the serialized command to a file containing the log
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .append(true)
+            .open(String::from(&self.filepath))
+            .unwrap();
+
+        // If it fails, it exits by printing the error and returning a non-zero error code
+        if let Err(e) = writeln!(file, "{}", cmd_string) {
+            eprintln!("Couldn't write to file: {}", e);
+            return Err(format_err!("Couldn't write to file: {}", e));
         }
+
+        // If that succeeds, it exits silently with error code 0
+        return Ok(());
     }
 
-    /// Removes the value and key in the store using the key.
-    /// # Examples
-    /// ```
-    /// use kvs::KvStore;
-    /// let mut kvs = KvStore::new();
-    ///
-    /// let key = String::from("foo");
-    /// let value = String::from("bar");
-    ///
-    /// kvs.set(key, value);
-    ///
-    /// kvs.rm(String::from("foo"));
-    /// ```
-    pub fn remove(&mut self, key: String) {
-        self.store.remove(&key);
+    pub fn get(&mut self, key: String) -> Result<Option<String>> {
+        // kvs reads the entire log, one command at a time, recording the affected key and file offset of the command to an in-memory key -> log pointer map
+        match KvStore::open(Path::new(".")) {
+            Ok(kvs) => { self.store = kvs.store }
+            Err(e) => return Err(format_err!("Couldn't open file to insert key value pars: {}", e))
+        }
+
+        // It then checks the map for the log pointer
+        let found_value = match self.store.get(&key) {
+            Some(val) => val,
+            // If it fails, it prints "Key not found", and exits with exit code 0
+            None => return Err(format_err!("Key not found")),
+        };
+
+        // If it succeeds
+        // It deserializes the command to get the last recorded value of the key
+        // It prints the value to stdout and exits with exit code 0
+        return Result::Ok(Some(String::from(found_value))); // Todo String::from is a hack, not sure how to do this better
+    }
+
+    pub fn remove(&mut self, key: String) -> Result<()> {
+        // Same as the "get" command, kvs reads the entire log to build the in-memory index
+        // It then checks the map if the given key exists
+        // If the key does not exist, it prints "Key not found", and exits with a non-zero error code
+        match self.get(key.clone()) { // Todo not sure if the clone is right
+            Ok(_) => {}
+            Err(_) => {
+                println!("Key not found");
+                std::process::exit(1);
+            }
+        }
+
+        // If it succeeds
+        // It creates a value representing the "rm" command, containing its key
+        let cmd = Command {
+            key,
+            value: String::new(),
+            kind: String::from("rm"),
+        };
+
+        let cmd_string = serde_json::to_string(&cmd)?;
+
+        // It then appends the serialized command to the log
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .append(true)
+            .open(String::from(&self.filepath))
+            .unwrap();
+
+        // If it fails, it exits by printing the error and returning a non-zero error code
+        if let Err(e) = writeln!(file, "{}", cmd_string) {
+            eprintln!("Couldn't write to file: {}", e);
+            return Err(format_err!("Couldn't write to file: {}", e));
+        }
+
+        // If that succeeds, it exits silently with error code 0
+        return Ok(());
+    }
+
+    pub fn open(path: impl Into<PathBuf>) -> Result<KvStore> {
+        let mut into_path = path.into();
+
+        if into_path.is_dir() {
+            into_path.push(FILE_NAME);
+        }
+
+        let filepath = into_path.into_os_string().into_string().unwrap();
+
+        let mut kvs = KvStore::new(filepath);
+
+        let file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(String::from(&kvs.filepath))
+            .unwrap();
+
+        for line in io::BufReader::new(file).lines() {
+            if let Ok(ip) = line {
+                // Todo: It should ideally not deserialize every line, only those of the matching key.
+                // Will make improvement.
+                let cmd: Command = serde_json::from_str(&ip.trim()).unwrap();
+
+                if cmd.kind == "set" {
+                    kvs.store.insert(cmd.key, cmd.value);
+                } else if cmd.kind == "rm" {
+                    kvs.store.remove(&cmd.key);
+                }
+            }
+        }
+
+        Ok(kvs)
     }
 }
